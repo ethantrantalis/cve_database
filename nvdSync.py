12b01285e0b517deb23933124a906d5a461cf10e
Ethan Trantalis <github@etcyber.org>
2025-05-26 00:56:05 -0700

    successful cve pull

---
+++ nvdSync.py
@@ -0,0 +1,81 @@
+import dotenv
+import os
+import requests
+import json
+import time
+
+# Load env for api key
+# ----> ADD A .env file with API_KEY="" <----
+dotenv.load_dotenv()
+apiKey = os.getenv("API_KEY")
+headers = {"apiKey": apiKey}
+baseURL = "https://services.nvd.nist.gov/rest/json/"
+
+# Helper to get the total results from an initial pull for future db initializations
+def getTotalResults(filePath: str) -> int:
+
+    # Return the value found at totalResults or -1 if error
+    try:
+        with open(filePath, "r") as f:
+            data = json.load(f)
+
+            return data["totalResults"]
+
+    except FileNotFoundError:
+        return -1
+
+# Helper function to call api for a variety of different api URLs
+def callAPI(url: str, startIndex: int, callType: str, resultsPerPage: int) -> int:
+
+    # Return new startIndex value after successful API call or -1
+    try:
+        response = requests.get(url, headers=headers)
+        response.raise_for_status()
+        data = response.json()
+        with open(f"{callType.lower()}_data/{callType.lower()}{startIndex}To{startIndex + resultsPerPage -1}.json", "w") as f:
+            json.dump(data, f, indent=2)
+
+        print(f"Successfully pulled {len(data.get('vulnerabilities', []))} {callType.lower()}s up to index {startIndex + resultsPerPage -1}.")
+
+        return startIndex + resultsPerPage  # Return the start index incremented by how many records were called
+
+    except requests.exceptions.HTTPError as e:
+        print(f"Error: {e}")
+
+        return -1  # Error return
+
+# Function to pull all records as per the rate limiter on nvd 50 calls per 30 secs w/ apiKey
+def getRecords(callType: str, resultsPerPage: int) -> None:
+
+    # Return None since helper functions handle errors
+
+    # According to the documentation, the max CVEs per request are 2000
+    # Set start to 0, pulling 2000 record increments
+    startIndex = 0
+    os.makedirs(f"{callType.lower()}_data", exist_ok=True)  # Make the output directory if not found
+    url = f"{baseURL}{callType.lower()}s/2.0/?resultsPerPage={resultsPerPage}&startIndex={startIndex}"
+    startIndex = callAPI(url, startIndex, callType, resultsPerPage)
+
+    if startIndex == -1:
+        print("Error: Failed to pull records.")
+        return
+
+    totalResults = getTotalResults(f"{callType.lower()}_data/{callType.lower()}{startIndex - resultsPerPage}To{startIndex -1}.json")
+    while startIndex < totalResults:
+
+        # Pause api calls to adhere to nvd rate limiter
+        time.sleep(1)
+
+        # Update URL with new startIndex from previous callAPI
+        url = f"{baseURL}{callType.lower()}s/2.0/?resultsPerPage={resultsPerPage}&startIndex={startIndex}"
+        startIndex = callAPI(url, startIndex, callType, resultsPerPage)
+
+
+        if startIndex == -1:
+            print("Error: Failed to pull records.")
+            break
+
+
+
+if __name__ == "__main__":
+    getRecords("CVE", 2000)
---
+++ parseCVEGithub.py
@@ -0,0 +1,92 @@
+import os
+import git
+from typing import TypeAlias
+
+# Global variables for local and remote repo
+repoURL = "https://github.com/CVEProject/cvelistV5"
+repoLocalPath = os.getcwd() + "/cvelistV5"
+repo = git.Repo(repoLocalPath)
+repo_origin = repo.remotes.origin
+directory_path = "cvelistV5/cves"
+
+# Traverse the cve directory and parse each json via preorder DFS
+def DFS(start: str) -> None:
+
+    # Utility function for DFS
+    def DFSutil(path: str, visited: set) -> None:
+
+        visited.add(path)
+        print(f"Visited {path}.")
+
+        # If the file is not a directory, dont read it
+        if not os.path.isdir(path):
+
+            # Parse json files here, json files are leaves in the dir tree
+            # ---- >  < ---- #
+            return
+
+        # If the file is a directory, read it and check if in set
+        for file in os.listdir(path):
+
+            full_path = os.path.join(path, file)
+
+            if full_path not in visited:
+                DFSutil(full_path, visited)
+
+    visited = set()
+
+    DFSutil(start, visited)
+
+
+# Compare latest commit from github repo
+def compareLocalRemoteCommits() -> dict[str, str]:
+    try:
+
+        # Get a commit object for the local repo head
+        localHead = repo.head.commit
+        print(f"Successfully found local head -> {localHead}.")
+
+        # Fetch to get latest updates, get a commit oject for remote repo head
+        repo_origin.fetch()
+        remoteHead = repo.refs['origin/main'].commit
+        print(f"Successfully found remote head -> {remoteHead}.")
+
+
+        # If remote has changes, diff and find changed items
+        if localHead != remoteHead:
+
+            print("Difference between local and remote, finding modified files.")
+
+            diff = remoteHead.diff(localHead)
+            diffItems: Dict[str, str] = {}
+
+            # Return a dictionary of created, modified, or deleted file paths to change type
+            for diff_item in diff:
+                print(f"{diff_item.a_path} -> {diff_item.change_type}")
+                diffItems[diff_item.a_path] = diff_item.change_type
+            print(f"Successfull found {len(diffItems)} changes.")
+
+            # Finally, pull updates into local since changes are tracked
+            repo_origin.pull()
+            print("Successfully pulled repository changes.")
+            return diffItems
+
+        # No changes found, return empty dict
+        print("Local repository is up to date, no new commits found.")
+        return {}
+
+
+    except subprocess.CalledProcessError as e:
+        print(e)
+        return {} # Treat error as no changes found
+
+
+
+
+if __name__ == "__main__":
+
+    compareLocalRemoteCommits()
+
+
+
+    # DFS(directory_path)
